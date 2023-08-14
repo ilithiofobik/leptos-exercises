8bfca24a9deeb8c7407189267a59346f0d048c8d
Wojciech Sęk <wojciech.sek17@gmail.com>
2023-08-14 20:16:36 +0200

    delete dist

--- src/utils/rand.rs
+++ src/utils/rand.rs
@@ -1,6 +1,5 @@
 /// Return a value of Geo(p) distribution
 pub fn geo(p: f64) -> usize {
     let u = fastrand::f64();
-    let q = 1.0 - p;
-    (u.log(q) + 1.0).floor() as usize
+    u.log(1.0 - p).floor() as usize + 1
 }
